29d900320179bcb9e9c2318777afba6bca2aeca7
Woodrow <wtscott@iastate.edu>
2017-04-28 18:54:58 -0500

    Menus

---
+++ Unity/Civis/Assets/Scripts/UI/ColorLerp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorLerp : MonoBehaviour
+{
+    public Color [] Colors;
+    public float Speed;
+
+    private int _currColor;
+    private int _nextColor;
+    private float _time;
+    private Text _text;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _currColor = 0;
+	    _nextColor = 1;
+
+        _text = GetComponent<Text>();
+        _text.color = Colors[0];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _time += Time.deltaTime * Speed;
+
+	    _text.color = Color.Lerp(Colors[_currColor], Colors[_nextColor], _time);
+
+	    if (_time >= 1)
+	    {
+	        _time = 0;
+
+	        _currColor = _currColor + 1 == Colors.Length ? 0 : _currColor + 1;
+            _nextColor = _currColor + 1 == Colors.Length ? 0 : _currColor + 1;
+	    }
+	}
+}
---
+++ Unity/Civis/Assets/Scripts/UI/MenuAnim.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuAnim : MonoBehaviour
+{
+    private List<GameObject> _panels;
+    private GameObject _current;
+
+	// Use this for initialization
+	void Start ()
+	{
+        _panels = new List<GameObject>();
+
+	    foreach (Transform child in transform)
+	    {
+	        if (child.tag == "UI_Panel")
+	        {
+	            _panels.Add(child.gameObject);
+                child.gameObject.SetActive(false);
+
+                if (child.name == "Front")
+	            {
+	                _current = child.gameObject;
+                    _current.SetActive(true);
+	            }
+            }
+	    }
+	}
+
+    public void SwitchTo(string menu)
+    {
+        foreach (var panel in _panels)
+        {
+            if (!panel.name.Equals(menu)) continue;
+
+            //enable selected panel, close current
+            //TODO add slide animation
+            _current.SetActive(false);
+            _current = panel;
+            _current.SetActive(true);
+
+            break;
+        }
+    }
+}
